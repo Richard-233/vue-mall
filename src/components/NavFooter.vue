ace85cbef7289869b72d86225810fb147dddba13
Richard Young <565435857@qq.com>
2022-03-05 10:54:25 +0800

    底部页面

--- src/components/NavFooter.vue
+++ src/components/NavFooter.vue
@@ -5,12 +5,12 @@
      <p>拼夕夕网上商城</p>
    </div>
    <div class="footer-link">
-     <a href="">拼多多首页</a><span>|</span>
-     <a href="">校园招聘</a><span>|</span>
-     <a href="">社会招聘</a><span>|</span>
-     <a href="">关于我们</a>
+     <a href="https://www.pinduoduo.com/">拼多多首页</a><span>|</span>
+     <a href="https://careers.pinduoduo.com/campus/">校园招聘</a><span>|</span>
+     <a href="https://careers.pinduoduo.com/jobs">社会招聘</a><span>|</span>
+     <a href="https://www.pinduoduo.com/home/about/">关于我们</a>
    </div>
-   <div class="copyright">Copyright ©2022 TEAM07 All Rights Reserved.</div>
+   <div class="copyright">Copyright ©2022 <span class="domain">TEAM07</span> All Rights Reserved.</div>
  </div>
 </template>
 
@@ -34,6 +34,22 @@ export default {
     img{
       width:53px;
       height:53px;
+      margin-bottom: 13px;
+    }
+  }
+  .footer-link{
+    a{
+      color:#999999;
+      display: inline-block;
+    }
+    span{
+      margin:0 10px;
+    }
+    margin-bottom: 13px;
+  }
+  .copyright{
+    .domain{
+      color: #e4291e;
     }
   }
 }
